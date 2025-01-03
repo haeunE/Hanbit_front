d08d65a0280b55541e44ea3618a68bf60f62533f
haeun <grace10043004@gmail.com>
2025-01-03 20:21:43 +0900

    FIX: 컨테이너 넣기, css 이름 수정, 비율고치기

--- src/components/main/Login.jsx
+++ src/components/main/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './User.css';
+import { Container } from 'react-bootstrap';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -12,39 +13,41 @@ const Login = () => {
   };
 
   return (
-    <div className="user-body">
-      <div className="login-container">
-        <h2 className='user_h2'>로그인</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label className='user_label' htmlFor="email">이메일:</label>
-            <input
-              className='user_input'
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className='user_label' htmlFor="password">비밀번호:</label>
-            <input
-              className='user_input'
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button className='user_button' type="submit">로그인</button>
-          <div className="signup-link">
-            <a href="/signup">회원가입</a>
-          </div>
-        </form>
+    <Container>
+      <div className="user-body">
+        <div className="login-container">
+          <h2 className='user_h2'>로그인</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label className='user_label' htmlFor="email">이메일:</label>
+              <input
+                className='user_input'
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label className='user_label' htmlFor="password">비밀번호:</label>
+              <input
+                className='user_input'
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button className='user_button' type="submit">로그인</button>
+            <div className="signup-link">
+              <a href="/signup">회원가입</a>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- src/components/main/Signup.jsx
+++ src/components/main/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './User.css';
 import axiosInstance from '../../axiosInstance';
+import { Container } from 'react-bootstrap';
 
 const Signup = () => {
   const [userForm, setSignupForm] = useState({
@@ -80,115 +81,117 @@ const Signup = () => {
   };
 
   return (
-    <div className="user-body">
-      <div className="signup-container">
-        <h2>회원가입</h2>
-        <form onSubmit={handleSubmit}>
-          {/* 입력 필드 */}
-          <div className="form-group">
-            <label className='user_label' htmlFor="username">아이디:</label>
-            <input
-              className='user_input'
-              type="text"
-              id="username"
-              name="username"
-              value={userForm.username}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className='user_label' htmlFor="password">비밀번호:</label>
-            <input
-              className='user_input'
-              type="password"
-              id="password"
-              name="password"
-              value={userForm.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className='user_label' htmlFor="email">이메일:</label>
-            <input
-              className='user_input'
-              type="email"
-              id="email"
-              name="email"
-              value={userForm.email}
-              onChange={handleChange}
-              required
-            />
-            
-          </div>
-          <button
-            className='user_button'
-            type="button"
-            onClick={handleEmailVerification}
-            disabled={emailVerificationPending || emailVerified}
-          >
-            {emailVerificationPending ? '발송 중...' : '인증 코드 발송'}
-          </button>
-          <div className="form-group">
-            <label className='user_label' htmlFor="verification">인증 코드:</label>
-            <input
-              className='user_input'
-              type="text"
-              id="verification"
-              value={inputCode}
-              onChange={(e) => setInputCode(e.target.value)}
-            />
-          </div>
-          <button
-            className='user_button'
-            type="button"
-            onClick={verifyCode}
-            disabled={emailVerified}
-          >
-            인증하기
-          </button>
-          {/* 추가 필드 */}
-          <div className="form-group">
-            <label className='user_label' htmlFor="name">이름:</label>
-            <input
-              className='user_input'
-              type="text"
-              id="name"
-              name="name"
-              value={userForm.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className='user_label' htmlFor="tel">비상 연락처:</label>
-            <input
-              className='user_input'
-              type="tel"
-              id="tel"
-              name="tel"
-              value={userForm.tel}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="user_label">외국인 여부:</label>
-            <div className='chk-box'>
-            <input
-              className='forign_check'
-              type="checkbox"
-              name="foreignYN"
-              checked={userForm.foreignYN}
-              onChange={handleChange}
-            />
+    <Container>
+      <div className="user-body">
+        <div className="signup-container">
+          <h2>회원가입</h2>
+          <form onSubmit={handleSubmit}>
+            {/* 입력 필드 */}
+            <div className="form-group">
+              <label className='user_label' htmlFor="username">아이디:</label>
+              <input
+                className='user_input'
+                type="text"
+                id="username"
+                name="username"
+                value={userForm.username}
+                onChange={handleChange}
+                required
+              />
             </div>
-          </div>
-          <button className='user_button' type="submit">회원가입</button>
-        </form>
+            <div className="form-group">
+              <label className='user_label' htmlFor="password">비밀번호:</label>
+              <input
+                className='user_input'
+                type="password"
+                id="password"
+                name="password"
+                value={userForm.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label className='user_label' htmlFor="email">이메일:</label>
+              <input
+                className='user_input'
+                type="email"
+                id="email"
+                name="email"
+                value={userForm.email}
+                onChange={handleChange}
+                required
+              />
+              
+            </div>
+            <button
+              className='user_button'
+              type="button"
+              onClick={handleEmailVerification}
+              disabled={emailVerificationPending || emailVerified}
+            >
+              {emailVerificationPending ? '발송 중...' : '인증 코드 발송'}
+            </button>
+            <div className="form-group">
+              <label className='user_label' htmlFor="verification">인증 코드:</label>
+              <input
+                className='user_input'
+                type="text"
+                id="verification"
+                value={inputCode}
+                onChange={(e) => setInputCode(e.target.value)}
+              />
+            </div>
+            <button
+              className='user_button'
+              type="button"
+              onClick={verifyCode}
+              disabled={emailVerified}
+            >
+              인증하기
+            </button>
+            {/* 추가 필드 */}
+            <div className="form-group">
+              <label className='user_label' htmlFor="name">이름:</label>
+              <input
+                className='user_input'
+                type="text"
+                id="name"
+                name="name"
+                value={userForm.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label className='user_label' htmlFor="tel">비상 연락처:</label>
+              <input
+                className='user_input'
+                type="tel"
+                id="tel"
+                name="tel"
+                value={userForm.tel}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label className="user_label">외국인 여부:</label>
+              <div className='chk-box'>
+              <input
+                className='forign_check'
+                type="checkbox"
+                name="foreignYN"
+                checked={userForm.foreignYN}
+                onChange={handleChange}
+              />
+              </div>
+            </div>
+            <button className='user_button' type="submit">회원가입</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
