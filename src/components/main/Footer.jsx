880b268f19eee4f7a0637e39f16c1c711956e1ab
hhhk <hakyeong0@naver.com>
2025-01-07 19:43:51 +0900

    Fix: intro, footer css 수정

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom"; // react-router-dom을 사용하여 페이지 이동
+import { useLocation } from "react-router-dom"; // react-router-dom을 사용하여 페이지 이동
 import "./Footer.css";
 
 function Footer() {
@@ -11,9 +11,9 @@ function Footer() {
   const footerLinks = [
     { url: "https://knto.or.kr/index", img: "/img/footer/ftlogo_knto.jpg", alt: "한국관광공사" },
     { url: "https://www.data.go.kr/index.do", img: "/img/footer/ftlogo_publicdata.png", alt: "publicdata" },
-    { url: "https://data.seoul.go.kr/", img: "/img/footer/ftlogo_seouldata.png", alt: "서울열린데이터광장" },
+    { url: "https://data.seoul.go.kr/", img: "/img/footer/ftlogo_seoulopendata.png", alt: "seoulopendata" },
     { url: "https://www.culture.go.kr/data/main/main.do", img: "/img/footer/ftlogo_culturedata.png", alt: "문화공공데이터광장" },
-    { url: "https://www.durunubi.kr/", img: "/img/footer/ftlogo_durunubi.jpg", alt: "두루누비" },
+    { url: "https://www.durunubi.kr/", img: "/img/footer/ftlogo_durunubi.jpg", alt: "durunubi" },
     { url: "https://api.visitkorea.or.kr/#/", img: "/img/footer/ftlogo_tourAPI.png", alt: "TourAPI 4.0" },
     { url: "https://mcst.go.kr/kor/main.jsp", img: "/img/footer/ftlogo_culture.jpg", alt: "문화체육관광부" },
     { url: "https://www.seoul.go.kr/main/index.jsp", img: "/img/footer/ftlogo_seoul.png", alt: "서울특별시" }
@@ -30,41 +30,33 @@ function Footer() {
     <div className="Footer">
       <div className="slide-container">
         <ul
-          className="slided-wrapper"
-          style={{ animationPlayState: animate ? "running" : "paused" }}
+          className="slide-wrapper"
+          style={{ animationPlayState: animate ? "" : "stop" }}
           onMouseEnter={onStop}
           onMouseLeave={onRun}
         >
-          <div className="slide-original">
-            {footerLinks.map((link, i) => (
-              <li key={i}>
-                <p className="item">
-                  <a href={Link.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      src={Link.img}
-                      alt={Link.alt}
-                      className={link.alt === "publicdata" ? "publicdata-img" : ""}
-                    />
-                  </a>
-                </p>
-              </li>
-            ))}
-          </div>
-          <div className="slide-clone">
-            {footerLinks.map((link, i) => (
-              <li key={i}>
-                <p className="item">
-                  <a href={link.url} target="_blank" rel="noopener noreferrer">
+          {/* 배열을 두 번 반복하여 무한 슬라이드 효과 구현 */}
+          {footerLinks.concat(footerLinks).map((link, i) => (
+            <li key={i}>
+              <p className="item">
+                <a href={link.url} target="_blank" rel="noopener noreferrer">
                   <img
-                  src={link.img}
-                  alt={link.alt}
-                  className={link.alt === "publicdata" ? "publicdata-img" : ""}
-                />
-                  </a>
-                </p>
-              </li>
-            ))}
-          </div>
+                    src={link.img}
+                    alt={link.alt}
+                    className={
+                      link.alt === "publicdata"
+                        ? "publicdata-img"
+                        : link.alt === "seoulopendata"
+                          ? "seoulopendata-img"
+                          : link.alt === "durunubi"
+                            ? "durunubi-img"
+                            : ""
+                    }
+                  />
+                </a>
+              </p>
+            </li>
+          ))}
         </ul>
       </div>
 
--- src/pages/jsx/intro.jsx
+++ src/pages/jsx/intro.jsx
@@ -5,50 +5,57 @@ import { LuSun } from "react-icons/lu";
 import { WiMoonWaxingCrescent4 } from "react-icons/wi";
 import { useDispatch } from 'react-redux';
 import { SetIsMode } from '../../redux/modeState';
+import { PiArrowBendDownLeftBold } from "react-icons/pi";
+import { PiArrowBendUpRightBold } from "react-icons/pi";
 
 function Intro() {
   const navigate = useNavigate();
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
   // 낮 모드
   const DayMode = () => {
     dispatch(SetIsMode(true));
-    localStorage.setItem("isMode", JSON.stringify(true)); 
+    localStorage.setItem("isMode", JSON.stringify(true));
     navigate('/home');
   };
-  
+
   //  밤 모드
   const NightMode = () => {
     dispatch(SetIsMode(false));
-    localStorage.setItem("isMode", JSON.stringify(false)); 
+    localStorage.setItem("isMode", JSON.stringify(false));
     navigate('/home');
   };
-  
+
 
   return (
     <div className='Intro'>
+      <div className='imageframe'>
+        <div className='top'>
+          <div className='logo-top'>
+            <img src='/img/logo/logo_intro.png' alt="Logo Image" className="image" ></img>
+          </div>
 
-      <div className='top'>
-        {/* <div className='logo-top'>
-          <img src='/img/logo/logo_intro.png' alt="Logo Image" className="image" ></img>
-        </div> */}
-
-        <div className='text-top'>
-          <p>Click the journey you'd like!<br />
-            원하시는 여정을 클릭하세요.</p>
-        </div>
-      </div>
-
-      <div className="image-overlay">
-
-        <div className="image-wrapper" onClick={DayMode}>
-          <img src="/img/intro/introday.jpg" alt="Day Image" className="image" />
-          <div className="overlay"><LuSun /> DAY</div>
+          <div className='text-top'>
+            <p>Click the journey you'd like!<br />
+              원하시는 여정을 클릭하세요.</p>
+          </div>
         </div>
 
-        <div className="image-wrapper" onClick={NightMode}>
-          <img src="/img/intro/intronight.jpg" alt="Night Image" className="image" />
-          <div className="overlay">NIGHT <WiMoonWaxingCrescent4 /></div>
+        <div className="image-overlay">
+          <div className="image-wrapper" onClick={DayMode}>
+            <img src="/img/intro/introday.jpg" alt="Day Image" className="image" />
+            <div className="overlay"><LuSun /> DAY</div>
+          </div>
+
+          <div className="middlebox">
+            <div className='dayclick'>DAY<br/>Click!<br/><PiArrowBendDownLeftBold /></div>
+            <div className='nigntclick'><PiArrowBendUpRightBold /><br/>NIGHT<br/>Click!</div>
+          </div>
+
+          <div className="image-wrapper" onClick={NightMode}>
+            <img src="/img/intro/intronight.jpg" alt="Night Image" className="image" />
+            <div className="overlay">NIGHT <WiMoonWaxingCrescent4 /></div>
+          </div>
         </div>
       </div>
     </div>
