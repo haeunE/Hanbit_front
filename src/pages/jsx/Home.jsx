3a8bc7d01708d003134460dcaa82ece4b35dd42d
hana <simhana0122@gmail.com>
2025-01-13 09:58:32 +0900

    fix : home page 코드state필요없는것 제거, 변수들 변화가 안되서  랜더링 안되는것 수정

--- src/pages/jsx/Home.jsx
+++ src/pages/jsx/Home.jsx
@@ -1,15 +1,15 @@
 import "../css/Home.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SetIsMode } from "../../redux/modeState";
+import { SetLanguage } from "../../redux/languageState";
 import Weather from "../../components/jsx/Weather";
 import Location from "../../components/jsx/Location";
 import { Container } from "react-bootstrap";
 import TripPlacesDay from "../../components/jsx/TripPlacesDay";
 import Festival from "../../components/jsx/Festival";
 import "@/locales/i18n";
-import i18n from 'i18next';  // i18n을 import
-import { SetLanguage } from "../../redux/languageState";
+import i18n from "i18next";
 import { useTranslation } from "react-i18next";
 
 function Home() {
@@ -17,97 +17,96 @@ function Home() {
   const isMode = useSelector((state) => state.isMode);
   const isLanguage = useSelector((state) => state.isLanguage);
   const dispatch = useDispatch();
-  
-  // 초기값을 useState로 설정
-  const [placeContentTypeId, setPlaceContentTypeId] = useState(12);
-  const [restaurantContentTypeId, setRestaurantContentTypeId] = useState(39);
-  const [hotelContentTypeId, setHotelContentTypeId] = useState(32);
-  
-  // 랜덤 페이지 번호 설정 (각각의 범위에 맞춰)
-  const [placePageNo, setPlacePageNo] = useState(Math.floor(Math.random() * 5) + 1); 
-  const [restaurantPageNo, setRestaurantPageNo] = useState(Math.floor(Math.random() * 20) + 1); 
-  const [hotelPageNo, setHotelPageNo] = useState(Math.floor(Math.random() * 2) + 1);
-  
+
+  // 콘텐츠 타입 ID 계산
+  const placeContentTypeId = isLanguage === "ko" ? 12 : 76;
+  const restaurantContentTypeId = isLanguage === "ko" ? 39 : 82;
+  const hotelContentTypeId = isLanguage === "ko" ? 32 : 80;
+
+  // 랜덤 페이지 번호 계산
+  const placePageNo = Math.floor(Math.random() * 5) + 1;
+  const restaurantPageNo = Math.floor(Math.random() * 20) + 1;
+  const hotelPageNo = Math.floor(Math.random() * 2) + 1;
 
   useEffect(() => {
+    // 로컬 스토리지에서 모드 및 언어 설정 불러오기
     const savedMode = JSON.parse(localStorage.getItem("isMode"));
-    if (savedMode) {
+    if (savedMode !== null) {
       dispatch(SetIsMode(savedMode));
     }
-  }, [dispatch]);
 
-  useEffect(() => {
-    // isLanguage 상태에 따라 contentTypeId 변경
-    if (isLanguage === "ko") {
-      setPlaceContentTypeId(12);  // 한국어일 때
-      setRestaurantContentTypeId(39);
-      setHotelContentTypeId(32);  // 한국어일 때 추천 음식
-    } else {
-      setPlaceContentTypeId(76);  // 다른 언어일 때
-      setRestaurantContentTypeId(82);
-      setHotelContentTypeId(80);  // 다른 언어일 때 추천 음식
+    const savedLanguage = localStorage.getItem("lang");
+    if (savedLanguage) {
+      i18n.changeLanguage(savedLanguage);
+      dispatch(SetLanguage(savedLanguage));
     }
-
-    // i18n 라이브러리에서 언어 변경
-    i18n.changeLanguage(isLanguage);
-
-  }, [isLanguage]);
+  }, []); // 초기 로드 시 한 번만 실행
 
   const changeMode = () => {
-    const savedMode = !isMode;
-    dispatch(SetIsMode(savedMode));
-    localStorage.setItem("isMode", JSON.stringify(savedMode));
+    const newMode = !isMode;
+    dispatch(SetIsMode(newMode));
+    localStorage.setItem("isMode", JSON.stringify(newMode));
   };
 
   return (
     <Container>
       <div className="home">
+        {/* 모드 변경 버튼 */}
         <div className={`change-mode ${isMode ? "day" : "night"}`}>
           <button
             className="home-change-mode-day"
             onClick={changeMode}
-            disabled={isMode} // isMode가 true일 때 DAY 버튼 비활성화
+            disabled={isMode}
           >
-            {t`home.day`}
+            {t("home.day")}
           </button>
           <button
             className="home-change-mode-night"
             onClick={changeMode}
-            disabled={!isMode} // isMode가 false일 때 NIGHT 버튼 비활성화
+            disabled={!isMode}
           >
-            {t`home.night`}
+            {t("home.night")}
           </button>
         </div>
+
+        {/* 위치 및 날씨 */}
         <div className="location-weather">
           <Location />
           <Weather />
         </div>
+
+        {/* 콘텐츠 렌더링 */}
         <div className="carousel"></div>
-        {isMode?
-        <div>
-          <div className="recommend-place">
-            <TripPlacesDay contentTypeId={placeContentTypeId} pageNo={placePageNo} />
+        {isMode && (
+          <div>
+            <div className="recommend-place">
+              <TripPlacesDay contentTypeId={placeContentTypeId} pageNo={placePageNo} />
+            </div>
+            <div className="population"></div>
+            <div className="homepage-buttom">
+              <div className="recommend-festival">
+                <div className="event-banner">
+                  {t("home.event")} / {t("home.performance")}
+                </div>
+                <Festival />
+              </div>
+              <div className="recommend-restaurant">
+                <div className="restaurant-banner">{t("home.recommended-restaurants")}</div>
+                <TripPlacesDay
+                  contentTypeId={restaurantContentTypeId}
+                  pageNo={restaurantPageNo}
+                />
+              </div>
+              <div className="recommend-hotel">
+                <div className="hotel-banner">{t("home.recommended-accommodation")}</div>
+                <TripPlacesDay
+                  contentTypeId={hotelContentTypeId}
+                  pageNo={hotelPageNo}
+                />
+              </div>
+            </div>
           </div>
-          <div className="population"></div>
-          <div className="homepage-buttom">
-            <div className="recommend-festival">
-              <div className="event-banner">{t`home.event`}/{t`home.performance`}</div>
-              <Festival />
-            </div>
-            <div className="recommend-restaurant">
-              <div className="restaurant-banner">{t`home.recommended-restaurants`}</div>
-              <TripPlacesDay contentTypeId={restaurantContentTypeId} pageNo={restaurantPageNo} />
-            </div>
-            <div className="recommend-hotel">
-              <div className="hotel-banner">{t`home.recommended-accommodation`}</div>
-              <TripPlacesDay contentTypeId={hotelContentTypeId} pageNo={hotelPageNo} />
-            </div>
-          </div>
-        </div>  
-        :
-        <div>
-        </div>  
-        }
+        )}
       </div>
     </Container>
   );
