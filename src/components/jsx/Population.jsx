c403bed2906bcb26b44f503231226c1e7912c60d
hhhk <hakyeong0@naver.com>
2025-01-14 20:12:38 +0900

    Feat: population 작업중

--- src/components/jsx/Population.jsx
+++ src/components/jsx/Population.jsx
@@ -15,13 +15,24 @@ function Population() {
   const [nearPlace, setSelectedLocation] = useState(null); // 지도에 표시할 선택된 지역
   const [allPlaces, setAllPlaces] = useState([]); // 서울시 주요 116곳 명소 정보
   const [region, setRegion] = useState(initialRegion); // 현재 지역 상태
+  const [recommendations, setRecommendations] = useState([]); // 검색 추천 목록
+
+  const [test, setTest] = useState({lat: JSON.parse(location).latitude, lon: JSON.parse(location).longitude}) // 사용자 위치 위경도
 
   // 서울시 실시간 인구 데이터 API URL
   const MAP_APIKEY = import.meta.env.VITE_POPULATION_API_KEY;
 
   // 데이터 fetching 함수 (인구 데이터)
-  const fetchPopulationData = async (region) => {
-    const POPULATION_MAP_API_URL = `http://openapi.seoul.go.kr:8088/${MAP_APIKEY}/json/citydata_ppltn/1/5/${region}`;
+  const fetchPopulationData = async (placeName) => {
+    console.log(placeName)
+    const place = findClosestPlace(placeName.lat, placeName.lon)
+    console.log(place)
+    const POPULATION_MAP_API_URL = `http://openapi.seoul.go.kr:8088/${MAP_APIKEY}/json/citydata_ppltn/1/5/천호역`;
+
+    // console.log('fetchPopulationData URL : ', POPULATION_MAP_API_URL);
+    // console.log('placeName : ', placeName)
+    // console.log('location : ', location)
+    // console.log('region : ', region)
 
     try {
       const response = await fetch(POPULATION_MAP_API_URL);
@@ -40,7 +51,8 @@ function Population() {
 
   // 장소 검색 함수
   const searchPlace = async (query) => {
-    const searchmap_url = `http://localhost:8888/searchmap?query=${encodeURIComponent(query)}`;
+    const searchmap_url = `https://api.naver.com/v1/searchmap?query=${encodeURIComponent(query)}`;
+    // const searchmap_url = `http://localhost:8888/searchmap?query=${encodeURIComponent(query)}`;
 
     try {
       const response = await fetch(searchmap_url);
@@ -48,29 +60,29 @@ function Population() {
         const data = await response.json();
         setPlaces(data.items);  // 검색된 장소 항목을 상태로 설정
       } else {
-        throw new Error("Failed to fetch data: " + response.statusText);
+        throw new Error(" 데이터 가져오기 실패 : " + response.statusText);
       }
     } catch (error) {
-      console.error("Error fetching place data:", error);
+      console.error("장소 데이터 로딩 중 오류 : ", error);
       setPlaces([]);  // 오류 발생 시 장소 리스트 초기화
     }
   };
 
-  useEffect(() => {   // CSV 파일 로딩
-    const PlacesData = async () => {
+  // CSV 파일 로딩
+  useEffect(() => {
+    const loadcsv = async () => {
       const response = await fetch('/data/서울시 주요 116장소명 목록.csv');
       const text = await response.text();
 
       Papa.parse(text, {
-        header: true,  // CSV의 첫 번째 행을 헤더로 사용
+        header: true,
         complete: (result) => {
           const places = result.data.map(place => ({
-            AREA_NM: place.AREA_NM,  // 한글 명소 이름
-            ENG_NM: place.ENG_NM,  // 영어 명소 이름
-            mapx: null, // 경도 초기화 (나중에 API로 받아올 예정)
-            mapy: null // 위도 초기화 (나중에 API로 받아올 예정)
+            AREA_NM: place.AREA_NM,
+            ENG_NM: place.ENG_NM,
+            mapx: null,  // 경도 (나중에 API로 받아올 예정)
+            mapy: null,  // 위도 (나중에 API로 받아올 예정)
           }));
-
           setAllPlaces(places);
         },
         error: (error) => {
@@ -79,7 +91,7 @@ function Population() {
       });
     };
 
-    PlacesData();
+    loadcsv();
   }, []);
 
   // 명소에 대한 경도, 위도 정보를 API로부터 받아오기
@@ -89,17 +101,16 @@ function Population() {
     try {
       const response = await fetch(geocodingUrl);
       const data = await response.json();
-      
+
       // 데이터가 존재하는 경우에만 경도 및 위도 업데이트
       if (data && data.results && data.results[0]) {
         const { x: mapx, y: mapy } = data.results[0].geometry;
         return new naver.maps.LatLng(parseFloat(mapy), parseFloat(mapx));
       } else {
-        console.error(`Unable to find coordinates for ${placeName}`);
         return null;
       }
     } catch (error) {
-      console.error("Error fetching coordinates:", error);
+      console.error("좌표를 가져오는 중 오류가 발생했습니다:", error);
       return null;
     }
   };
@@ -107,38 +118,36 @@ function Population() {
   // 입력값에 맞는 장소 추천 (검색어와 일치하는 명소 추천)
   const getSearchRecommendations = (query) => {
     const lowerQuery = query.toLowerCase();  // 입력된 검색어를 소문자로 변환하여 비교
-    const recommendations = allPlaces.filter(place => 
-      place.AREA_NM.toLowerCase().includes(lowerQuery) || 
+    const recommendations = allPlaces.filter(place =>
+      place.AREA_NM.toLowerCase().includes(lowerQuery) ||
       place.ENG_NM.toLowerCase().includes(lowerQuery)
     );
     setRecommendations(recommendations);
   };
 
   // 입력된 지역에 가장 가까운 명소 찾기
-  const findClosestPlace = (query) => {
-    const inputLocation = allPlaces.find(place => place.AREA_NM === query);
-  
-    // inputLocation이 없으면 가장 가까운 명소 찾기
-    if (!inputLocation) {
-      let closestPlace = null;
-      let closestDistance = Infinity;
-  
-      allPlaces.forEach(place => {
-        // 입력 지역이 명소 목록에 없을 경우 거리 계산
-        const placeLatLng = new naver.maps.LatLng(parseFloat(place.mapy), parseFloat(place.mapx));
-        const inputLatLng = new naver.maps.LatLng(parseFloat(region.mapy), parseFloat(region.mapx));
-        const distance = naver.maps.LatLng.computeDistance(inputLatLng, placeLatLng);  // 네이버 지도 API의 거리 계산 함수 사용
-        
-        if (distance < closestDistance) {
-          closestDistance = distance;
-          closestPlace = place;
-        }
-      });
-  
-      return closestPlace;
-    } else {
-      return inputLocation;
-    }
+  const findClosestPlace = (latitude, longitude) => {
+    let closestPlace = null;
+    let closestDistance = Infinity;
+
+    allPlaces.forEach(place => {
+      console.log(place)
+      const placeLatLng = new naver.maps.LatLng(parseFloat(place.mapy), parseFloat(place.mapx));
+      const inputLatLng = new naver.maps.LatLng(latitude, longitude);
+      const distance = naver.maps.LatLng.computeDistance(inputLatLng, placeLatLng);  // 네이버 지도 API의 거리 계산 함수 사용
+
+      if (distance < closestDistance) {
+        closestDistance = distance;
+        closestPlace = place;
+      }
+    });
+
+    console.log('--------------')
+    console.log(allPlaces)
+    console.log(latitude, longitude);
+    console.log(closestPlace)
+
+    return closestPlace;
   };
 
   // 검색 버튼 클릭 시 검색
@@ -148,9 +157,13 @@ function Population() {
     await searchPlace(searchQuery);
 
     // 입력된 지역이 명소 목록에 없으면, 가장 가까운 명소 찾기
-    const closestPlace = findClosestPlace(searchQuery);
-    if (closestPlace) {
-      setSelectedLocation(closestPlace); // 가장 가까운 명소 위치 설정
+    const coordinates = await fetchCoordinates(searchQuery);
+    if (coordinates) {
+      const closestPlace = findClosestPlace(coordinates.lat(), coordinates.lng());
+      if (closestPlace) {
+        setSelectedLocation(closestPlace); // 가장 가까운 명소 위치 설정
+        fetchPopulationData(closestPlace.AREA_NM); // 가장 가까운 명소의 AREA_NM을 실시간 인구 데이터 API에 전달
+      }
     }
   };
 
@@ -169,9 +182,9 @@ function Population() {
   };
 
   // 검색어 변경 시 실시간 인구 데이터 업데이트
-  useEffect(() => { 
-    fetchPopulationData(region);  // 새로 설정된 지역에 대해 데이터 요청
-  }, [region]);
+  useEffect(() => {
+    fetchPopulationData(test);  // 새로 설정된 지역에 대해 데이터 요청
+  }, [allPlaces]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -181,24 +194,27 @@ function Population() {
     return <div>{error}</div>;
   }
 
+
   return (
     <div className="population">
 
       <div className='search-population'>
         <p> * 서울시 116개 주요 명소의 실시간 인구 데이터를 검색하실 수 있습니다.<br />
-            * 찾으시는 지역의 데이터가 없는 경우, 해당 지역과 가장 가까운 명소의 데이터를 검색합니다.<br />
-            * 서울시 이외의 데이터는 불러올 수 없습니다.</p>
+          * 찾으시는 지역의 데이터가 없는 경우, 해당 지역과 가장 가까운 명소의 데이터를 검색합니다.<br />
+          * 서울시 이외의 데이터는 불러올 수 없습니다.</p>
+
         <div className="search-box">
           <strong>장소 검색 : </strong>
           <input
             type="search"
             value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
+            onChange={handleInputChange}
             onKeyDown={handleKeyDown}  // 엔터키 검색
             placeholder="ex) 김포공항, 성수카페거리, 인사동"
           />
           <button onClick={handleSearch}>검색</button>
-          </div>
+        </div>
+
         {recommendations.length > 0 && (
           <div className="recommendations">
             <ul>
@@ -238,27 +254,33 @@ const MapWithMarker = ({ places, nearPlace }) => {
   const navermaps = useNavermaps();
   const defaultLocation = new navermaps.LatLng(37.5365, 127.1242);
 
+  // 명소 이름을 한글/영어에 상관없이 표시할 수 있도록 처리
+  const getPlaceTitle = (place) => {
+    // 영어 이름이 있으면 영어 이름을, 없으면 한글 이름을 사용
+    return place.ENG_NM || place.AREA_NM;
+  };
+
   return (
     <NaverMap defaultCenter={defaultLocation} defaultZoom={12}>
       {places.length > 0 ? (
         places.map((place, index) => {
-          const { title, mapx, mapy } = place;
+          const { mapx, mapy } = place;
           const lat = parseFloat(mapy);  // 위도 (latitude)
           const lng = parseFloat(mapx);  // 경도 (longitude)
           const location = new navermaps.LatLng(lat, lng);
 
           return (
-            <Marker key={index} position={location} title={title} />
-          )})
+            <Marker key={index} position={location} title={getPlaceTitle(place)} />
+          );
+        })
       ) : (
-        <Marker defaultPosition={defaultLocation} title="검색된 장소 없음" />
-      )}
-      
-      {nearPlace && (
-        <Marker
-          position={new navermaps.LatLng(parseFloat(nearPlace.mapy), parseFloat(nearPlace.mapx))}
-          title={nearPlace.AREA_NM}
-        />
+        // 검색된 장소가 없으면 가장 가까운 명소를 표시
+        nearPlace && (
+          <Marker
+            position={new navermaps.LatLng(parseFloat(nearPlace.mapy), parseFloat(nearPlace.mapx))}
+            title={getPlaceTitle(nearPlace)}
+          />
+        )
       )}
     </NaverMap>
   );
