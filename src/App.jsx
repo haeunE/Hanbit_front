a1d32e0dc16676a9a6788b4bb7df5583bc2e4984
hana <simhana0122@gmail.com>
2025-01-16 10:31:33 +0900

    fix : add.jsx 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,13 @@ import Home from './pages/jsx/Home'
 import Bicycle from './pages/jsx/Bicycle'
 import PlaceDetail from './pages/jsx/PlaceDetail'
 import MyReviews from './pages/jsx/MyReviews'
+import SeoulPage from './pages/jsx/SeoulPage'
 
 import {login, logout} from './redux/userState'
 import { SetIsMode } from './redux/modeState'
 import { clearAllStorage } from './utils/clearAllStorage'
-
 import { useDispatch, useSelector } from 'react-redux'
 import Cookies from 'js-cookie';
-import PlaceDetail from './pages/jsx/PlaceDetail'
-import SeoulPage from './pages/jsx/SeoulPage'
-
-
 
 
 
