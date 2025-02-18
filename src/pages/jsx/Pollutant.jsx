868220cdf80e2afb7193f93ebf988dc7d11e2d31
haeun <grace10043004@gmail.com>
2025-02-18 09:36:19 +0900

    FIX: 밤버전 길찾기 수정

--- src/pages/jsx/Pollutant.jsx
+++ src/pages/jsx/Pollutant.jsx
@@ -12,33 +12,33 @@ function Pollutant({cityAir}) {
         <div className="grid-container">
           <section className='box1'>
             <b className='title1'>PM2.5</b>
-            <p className='subtitle'>지름 2.5 마이크로 미만의 미세먼지</p>
-            <p className='value'>●{cityAir.pm25}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>지름 2.5 마이크로 미만의 미세먼지</div>
+            <div className='value'>●{cityAir.pm25}&micro;g/m<sup>3</sup></div>
           </section>
           <section className='box2'>
             <b className='title1'>PM10</b>
-            <p className='subtitle'>지름 10 마이크로 미만의 미세먼지</p>
-            <p className='value'>●{cityAir.pm10}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>지름 10 마이크로 미만의 미세먼지</div>
+            <div className='value'>●{cityAir.pm10}&micro;g/m<sup>3</sup></div>
           </section>
           <section className='box3'>
             <b className='title1'>O<sub>3</sub></b>
-            <p className='subtitle'>오존</p>
-            <p className='value'>●{cityAir.o3}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>오존</div>
+            <div className='value'>●{cityAir.o3}&micro;g/m<sup>3</sup></div>
           </section>
           <section className='box4'>
             <b className='title1'>NO<sub>2</sub></b>
-            <p className='subtitle'>이산화질소</p>
-            <p className='value'>●{cityAir.no2}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>이산화질소</div>
+            <div className='value'>●{cityAir.no2}&micro;g/m<sup>3</sup></div>
           </section>
           <section className='box5'>
             <b className='title1'>SO<sub>2</sub></b>
-            <p className='subtitle'>아황산가스</p>
-            <p className='value'>●{cityAir.so2}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>아황산가스</div>
+            <div className='value'>●{cityAir.so2}&micro;g/m<sup>3</sup></div>
           </section>
           <section className='box6'>
             <b className='title1'>CO</b>
-            <p className='subtitle'>일산화탄소</p>
-            <p className='value'>●{cityAir.co}&micro;g/m<sup>3</sup></p>
+            <div className='subtitle'>일산화탄소</div>
+            <div className='value'>●{cityAir.co}&micro;g/m<sup>3</sup></div>
           </section>
         </div>
       </section>
